cfc568df0a5811102f2672c9d8f65dac992d8cee
onepisYa <pis1@qq.vom>
2021-09-21 02:19:08 +0800

    update python script

--- get_my_starred.py
+++ get_my_starred.py
@@ -1,42 +1,92 @@
 import requests
+from datetime import datetime
+from datetime import timedelta
 
 fields = [
-    'name', 'language','full_name', 'stargazers_count', 'description',  'updated_at', 'homepage','clone_url',
+    'name', 'language', 'full_name', 'stargazers_count', 'description',
+    'updated_at', 'homepage', 'clone_url', 'html_url'
 ]
+template = '''
+<table>
+            <dl>
+              <dt><a href="{html_url}"> {name} </a></dt>
+              <dd>
+              {description}
+              </dd>
+            </dl>
+            <tbody>
+                <tr>
+                    <td><b>language</b></td>
+                    <td>{language}</td>
+                </tr>
+                <tr>
+                    <td><b>full_name</b></td>
+                    <td>{full_name}</td>
+                </tr>
+                <tr>
+                    <td><b>stargazers_count</b></td>
+                    <td>{stargazers_count}</td>
+                </tr>
+                <tr>
+                    <td><b>homepage</b></td>
+                    <td>{homepage}</td>
+                </tr>
+                <tr>
+                    <td><b>clone_url</b></td>
+                    <td>{clone_url}</td>
+                </tr>
+                <tr>
+                    <td><b>updated_at(china_local_time)</b></td>
+                    <td>{china_lt}</td>
+                </tr>
+                <tr>
+                    <td><b>updated_at(utc)</b></td>
+                    <td>{updated_at}</td>
+                </tr>
+                <tr>
+                    <td><b>page</b></td>
+                    <td>{page}</td>
+                </tr>
+            </tbody>
+</table>
+<hr>
 
-table_header = """| {name} | {language}| {full_name} | {stargazers_count}  | {description}  |\
-{updated_at} | {homepage} | {clone_url} |{page}|
-|------|-----------|----------|-------------|-----------|------------------|----------|------------|-------------------------|
-""".format(**{field:field for field in fields}, page='page')
-
-template ="| {name} | {language} | {full_name} | {stargazers_count}  | {description} |\
-{updated_at} | {homepage} | {clone_url} |{page}|\n"
-
+'''
 
 
 def get_pages(pages=1):
     """
     :pages: total_page
     """
-    url = "https://api.github.com/users/onepisya/starred?page={}&sort=starred&per_page=100"
+    url = """
+    https://api.github.com/users/onepisya/starred?page={}&sort=starred&per_page=100
+    """.strip()
+
     payload = {}
     headers = {}
-    star_pages = (requests.request("GET", url.format(i), headers=headers,
-                                   data=payload).json() for i in range(1, pages+1))
+    star_pages = (requests.request("GET",
+                                   url.format(i),
+                                   headers=headers,
+                                   data=payload).json()
+                  for i in range(1, pages + 1))
     return star_pages
 
 
 def process_pages(star_pages):
     for idx, page in enumerate(star_pages):
         for repo in page:
-            project = {field: str(repo.get(field, '无')).replace('|', "∧") for field in fields}
-            project.update({'page': idx+1})
-            project['name'] = make_img(repo.get('name'), repo.get('html_url'))
+            project = {
+                field: str(repo.get(field, '无')).replace('|', "∧")
+                for field in fields
+            }
+            project.update({'page': idx + 1})
+            timestamp = datetime.strptime(project["updated_at"],
+                                          "%Y-%m-%dT%H:%M:%SZ")
+            cn_lt = timestamp + timedelta(hours=8)
+            # 计算出中国的本地时间
+            project['china_lt'] = datetime.strftime(cn_lt, "%x %X")
+
             yield project
-
-
-def make_img(name, url):
-    return f"""[{name}]({url})"""
 
 
 def save_pages(projects):
@@ -46,16 +96,15 @@ show me my starred repo
 
 ---
 
-
 """)
-        fp.write(table_header)
         for project in projects:
             fp.write(template.format(**project))
+
 
 def main():
     pages = get_pages(15)
     project_gen = process_pages(pages)
-    projects = sorted(project_gen,key=lambda x: x['language'],reverse=False)
+    projects = sorted(project_gen, key=lambda x: x['language'], reverse=False)
     save_pages(projects)
 
 
